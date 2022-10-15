5a9ae9d244012db34b5dca1bd4002fb5b328c84d
vincent <vincenthambaya@gmail.com>
2022-10-15 21:45:18 +0700

    feat(success component ongoing)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import MainRoutes from "./views/MainRoutes";
-
+import Success from "./views/Success";
 function App() {
-	return <MainRoutes />;
+	// return <MainRoutes />;
+  return (
+    <div className="max-w-[600px] mx-auto">
+      <Success />
+    </div>
+  )
 }
 
 export default App;
