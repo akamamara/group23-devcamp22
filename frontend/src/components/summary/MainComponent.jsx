af7b4b607a3d6316c56c3d96a5413342042395c3
vincent <vincenthambaya@gmail.com>
2022-10-15 13:12:27 +0700

    feat: summary component ongoing

---
+++ frontend/src/components/summary/MainComponent.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import dummy from "../../assets/user.png";
+export default function MainComponent({ type }) {
+  if (type === "tukang") {
+    return (
+      <div className="space-y-3 flex flex-col">
+        <h1>Nama Mitra mekanik</h1>
+        <div className="flex gap-4">
+          <img src="https://api.iconify.design/material-symbols:location-on-outline.svg?color=%23888888" />
+          <p>Cibanteng dramaga bogor</p>
+        </div>
+        <div className="flex gap-4">
+          <img src="https://api.iconify.design/cil:money.svg?color=%23888888" />
+          <p>Rp. 5.000.000,00 - Rp. 8.000,000,00</p>
+        </div>
+        <button className="bg-blue-500 px-5 py-2 text-sm items-end text-white rounded-lg">
+          Setuju dengan ini
+        </button>
+      </div>
+    );
+  }
+
+  if (type === "material") {
+    return (
+      <div className="border-y border-slate-300 py-6 mt-5 gap-6 grid grid-cols-2">
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        <div className="flex col-span-1 gap-4">
+          <div className="flex-1">
+            <img src={dummy} className="" />
+          </div>
+          <div className="flex-[3]">
+            <p>5kg-Cat aviant</p>
+            <span>@Rp. 20.000</span>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
